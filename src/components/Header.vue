e90b0c7c4355edbef2882306c36a67ec8f0c3183
Viveks-Practice <viveks_@hotmail.com>
2021-04-07 17:48:42 -0700

    Removed extraneous 'Add Task' button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,16 @@
 <template>
     <header class="flex justify-between items-center mb-5 text-3xl">
         <h1>{{ title }}</h1>
-        <Button text="Add Task" color="green"/>
     </header>
 </template>
 
 <script>
-import Button from './Button'
 
 export default {
     name: 'Header',
     props: {
         title: String,
-    },
-    components: {
-        Button,
-    },
+    }
 }
 </script>
 
